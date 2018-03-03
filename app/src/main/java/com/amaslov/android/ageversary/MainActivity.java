fcd0d3d87bd4ee0a0c9590f453738f306353faa3
Artur Maslov <aartasas@gmail.com>
2018-03-03 23:12:43 +0200

    Implemented current year progress with seekarc library

--- app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
+++ app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
@@ -4,8 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.amaslov.android.ageversary.databinding.ActivityMainBinding;
+import com.triggertrap.seekarc.SeekArc;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setupActionBar();
+        setActionBar();
+        setYearProgress(mainBinding.saYearProgress, mainBinding.tvYearProgress);
     }
 
-    private void setupActionBar () {
+    private void setActionBar () {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_custom);
+    }
+
+    private void setYearProgress(SeekArc sa, TextView tv) {
+        final int DAYS_YEAR = 365;
+        final int ANGLE_MAX = 360;
+        int currentDayOfYear = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+        int percentOfYear = 100 * currentDayOfYear / DAYS_YEAR;
+        if (currentDayOfYear > ANGLE_MAX) {
+            sa.setSweepAngle(360);
+        } else {
+            sa.setSweepAngle(currentDayOfYear);
+        }
+        String percentFinal =
+                String.valueOf(percentOfYear) + getResources().getString(R.string.percent);
+        tv.setText(percentFinal);
     }
 }
