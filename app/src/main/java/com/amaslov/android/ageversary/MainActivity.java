22cf6f8f506396b1c0cc1ab9ac800f6f0ad9d15e
Artur Maslov <aartasas@gmail.com>
2018-03-04 22:36:54 +0200

    Add animation for year progress

--- app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
+++ app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 import com.amaslov.android.ageversary.databinding.ActivityMainBinding;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setActionBar();
         setYearProgress(mainBinding.saYearProgress, mainBinding.tvYearProgress);
+        setAnimations();
     }
 
     private void setActionBar () {
@@ -42,5 +48,15 @@ public class MainActivity extends AppCompatActivity {
         String percentFinal =
                 String.valueOf(percentOfYear) + getResources().getString(R.string.percent);
         tv.setText(percentFinal);
+    }
+
+    private void setAnimations(){
+        View saYear = mainBinding.saYearProgress;
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setStartOffset(500);
+        fadeIn.setDuration(750);
+        fadeIn.setRepeatMode(Animation.REVERSE);
+        saYear.setAnimation(fadeIn);
     }
 }
