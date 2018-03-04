6fdc0822f3ecc96c35b5af137091da26ec7cbe24
Artur Maslov <aartasas@gmail.com>
2018-03-04 23:57:31 +0200

    Add Google+ authentication

--- app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
+++ app/src/main/java/com/amaslov/android/ageversary/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amaslov.android.ageversary;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,14 @@ import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 import com.amaslov.android.ageversary.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
 import com.triggertrap.seekarc.SeekArc;
 
 import java.util.Calendar;
@@ -20,6 +29,8 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 141;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,53 @@ public class MainActivity extends AppCompatActivity {
         setActionBar();
         setYearProgress(mainBinding.saYearProgress, mainBinding.tvYearProgress);
         setAnimations();
+
+        this.mainBinding.ivProfileHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+                mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d(getLocalClassName(), "handleSignInResult: " + account.getPhotoUrl());
+            Picasso.with(this)
+                    .load(account.getPhotoUrl())
+                    .resize(85, 85)
+                    .centerCrop()
+                    .into(mainBinding.ivProfileHolder);
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(getLocalClassName(), "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void setActionBar () {
